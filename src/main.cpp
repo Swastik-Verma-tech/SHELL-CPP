c82b86f1690589ccb001fda5f311c9c80c95798d
Swastik-Verma <swastikjee09@gmail.com>
2025-12-04 22:14:34 +0530

    4th

--- src/main.cpp
+++ src/main.cpp
@@ -9,15 +9,27 @@ int main() {
 
   // TODO: Uncomment the code below to pass the first stage
   cout << "$ ";
+
+
+  // i had to write only one command not found
   string cmd;
   getline(cin,cmd);
   cout<<cmd<<": command not found\n";
 
+
+
+  // i had to write the command not found until user doesn't stop
   cout<<"$ ";
   while(true){
     string cmd1;
     getline(cin,cmd1);
+
+    if(cmd1=="exit") break; // implementing the exit builtin
+
     cout<<cmd1<<": command not found\n";
     cout<<"$ ";  
   }
+
+
+
 }
