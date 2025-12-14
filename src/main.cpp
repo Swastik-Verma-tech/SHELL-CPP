47d73683bc8f0705aa1cf96c895ac3d0caa2eef2
Swastik-Verma <swastikjee09@gmail.com>
2025-12-14 14:29:58 +0530

    37th

--- src/main.cpp
+++ src/main.cpp
@@ -113,7 +113,7 @@ int main() {
     getline(cin,cmd1);
     ofstream fout;
     auto idx=cmd1.find('>');
-    if(idx!=npos){
+    if(idx!=string::npos){
       string file_name=cmd1.substr(idx+2);
       fout.open(file_name);
       cmd1=cmd1.substr(0,idx-1);
@@ -248,7 +248,7 @@ int main() {
     // what i have got in output_final is something that i have to store if i got > in input
     // cout<<output_final;
     
-    if(idx!=npos){
+    if(idx!=string::npos){
       fout<<output_final;
       fout.close();
     }
