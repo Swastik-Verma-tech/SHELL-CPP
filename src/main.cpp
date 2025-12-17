54f18c0864654635b81f63959684129b7ff4f266
Swastik-Verma <swastikjee09@gmail.com>
2025-12-17 20:36:57 +0530

    41th

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
-#include <bits/stdc++.h>
+# include <bits/stdc++.h>
 # include <filesystem>
 # include <sys/wait.h>
+# include<fcntl.h>
+# include<unistd.h>
 
 using namespace std;
 namespace fs = filesystem;
@@ -110,6 +112,18 @@ int main() {
     output_final="";
     string cmd1;
     getline(cin,cmd1);
+
+    auto idx=cmd1.find('>');
+
+    if(idx!=string::npos){
+      string file_name=cmd1.substr(idx+2);
+      cmd1=cmd1.substr(0,idx-1);
+
+      auto fd_required=open(file_name, O_WRONLY | O_CREAT | O_TRUNC,0644);
+      dup2(fd_required,1);
+      close(fd);
+    }
+
     stringstream ss(cmd1);
     string word;
     ss>>word;
